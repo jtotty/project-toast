057da59acaef36a67669072f1b8eab89f581b70c
James Totty <james@nimblehq.co>
2024-12-20 11:57:06 +0700

    L06

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,9 +1,16 @@
 import React from "react";
+import useKeydown from "../../hooks/useKeydown";
 
 export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
+
+  const handleEscape = React.useCallback(() => {
+    setToasts([]);
+  }, []);
+
+  useKeydown("Escape", handleEscape);
 
   function createToast(message, variant) {
     setToasts([
@@ -24,20 +31,6 @@ function ToastProvider({ children }) {
       setToasts([...toasts]);
     }
   }
-
-  React.useEffect(() => {
-    function handleKeydown(event) {
-      if (event.key === "Escape") {
-        setToasts([]);
-      }
-    }
-
-    window.addEventListener("keydown", handleKeydown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeydown);
-    };
-  }, []);
 
   return (
     <ToastContext.Provider value={{ toasts, createToast, dismissToast }}>
